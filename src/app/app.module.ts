a73c8084d1701941bce6d119dbefc68a557fd3f7
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-06-20 18:25:14 +0200

    add bottom-modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GardenComponent } from './garden/garden.component';
 import { HeaderComponent } from './header/header.component';
+import { BottomModalComponent } from './bottom-modal/bottom-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GardenComponent,
-    HeaderComponent
+    HeaderComponent,
+    BottomModalComponent
   ],
   imports: [
     BrowserModule,
