9655492c580e91638f3b31d84ad35f557891d829
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-06-21 13:56:44 +0200

    add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyGardenComponent } from './my-garden/my-garden.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'mijn-tuin', component: MyGardenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
