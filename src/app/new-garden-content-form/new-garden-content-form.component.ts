e4aae91595f545aafcc257c691b4c56ede7aacad
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-06-23 13:52:48 +0200

    pass values to modal

--- src/app/new-garden-content-form/new-garden-content-form.component.ts
+++ src/app/new-garden-content-form/new-garden-content-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-new-garden-content-form',
@@ -9,9 +9,18 @@ export class NewGardenContentFormComponent implements OnInit {
 
   constructor() { }
 
+  @Input()
+  gridLocationX!: number;
+
+  @Input()
+  gridLocationY!: number;
+
   saveNewPlant() {
+    // TODO save actie voor jiuste coordinaten
+    // TODO event terug emitten om dialoog te sluiten
     console.log("nieuwe plant ")
   }
+
   ngOnInit(): void {
   }
 
