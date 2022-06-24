8cd65ceae765649fdf9c28bef39d981edf58b315
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-06-24 11:29:56 +0200

    add gardencontent endpoint

--- src/app/garden.service.ts
+++ src/app/garden.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Garden } from './garden';
+import { GardenContent } from './gardencontent';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -12,6 +13,10 @@ export class GardenService {
 
   getGarden(id: number): Observable<Garden> {
     return this.http.get<Garden>('http://localhost:8080/garden/' + id);
+  }
+
+  getGardenContent(id: number): Observable<GardenContent> {
+    return this.http.get<GardenContent>('http://localhost:8080/gardencontent/garden/' + id);
   }
 
   save(garden: Garden) {
