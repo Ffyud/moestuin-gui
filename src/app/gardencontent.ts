64ad3f3bb48cc4893238467abe86dcae167baf69
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-06-27 21:12:11 +0200

    expand gardencontent model

--- src/app/gardencontent.ts
+++ src/app/gardencontent.ts
@@ -1,5 +1,14 @@
+import { Plant } from "./plant";
+import { Garden } from "./garden";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+  })
 export class GardenContent {
     id!: number;
-    positionY!: number;
-    positionX!: number;
+    positionY: number = 0;
+    positionX: number = 0;
+    plant!: Plant;
+    garden!: Garden;
 }
