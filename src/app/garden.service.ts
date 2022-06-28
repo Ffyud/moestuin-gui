77fa9117d076d3c5a459892dfcfb7397de006053
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-06-28 14:09:41 +0200

    add delete operation

--- src/app/garden.service.ts
+++ src/app/garden.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpStatusCode } from '@angular/common/http';
 import { Garden } from './garden';
 import { GardenContent } from './gardencontent';
 import { Observable } from 'rxjs';
@@ -15,14 +15,16 @@ export class GardenService {
     return this.http.get<Garden>('http://localhost:8080/garden/' + id);
   }
 
-  // TODO Hoort misschien in aparte gardenContent service
-  getGardenContent(id: number): Observable<GardenContent[]> {
-    return this.http.get<GardenContent[]>('http://localhost:8080/gardencontent/garden/' + id);
+  deleteGardenContent(gardenId: number, locationX: number, locationY: number): Observable<HttpStatusCode> {
+    return this.http.delete<HttpStatusCode>('http://localhost:8080/gardencontent/garden/' + gardenId + '/' + locationX + '/' + locationY)
+  }
+
+  getGardenContent(gardenId: number): Observable<GardenContent[]> {
+    return this.http.get<GardenContent[]>('http://localhost:8080/gardencontent/garden/' + gardenId);
   }
 
   saveGardenContent(gardenContent: GardenContent) {
     return this.http.post('http://localhost:8080/gardencontent/', gardenContent);
-
   }
 
   save(garden: Garden) {
