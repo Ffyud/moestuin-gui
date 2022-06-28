ed0a5df0af85172b3b022d66275eb8502a1f331c
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-06-28 16:23:32 +0200

    add zaaikalender probeersel

--- src/app/my-garden-overview/my-garden-overview.component.ts
+++ src/app/my-garden-overview/my-garden-overview.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GardenService } from '../garden.service';
+import { GardenContent } from '../gardencontent';
 
 @Component({
   selector: 'app-my-garden-overview',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyGardenOverviewComponent implements OnInit {
 
-  constructor() { }
+  gardenContentArray!: GardenContent[];
+  monthsOfYear: Array<String> = ["jan", "feb", "mrt", "apr", "mei", "jun", "jul",
+    "aug", "sep", "okt", "nov", "dec"];
+  
+  constructor(private gardenService: GardenService) { }
 
+  // TODO maak van een string een array
+
+  getMonthActionSeed(seedPeriod: string, monthIndex: number) {
+    if(seedPeriod.includes(monthIndex.toString())) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  getMonthActionGround(plantPeriod: string, monthIndex: number) {
+    if(plantPeriod.includes(monthIndex.toString())) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  getMonthActionHarvest( harvestPeriod: string, monthIndex: number) {
+    if(harvestPeriod.includes(monthIndex.toString())) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  gardenId: number = 1; // FIXME hardcoded gardenId lol wut
+  
   ngOnInit(): void {
+    this.gardenService.getGardenContent(this.gardenId).subscribe(
+      data => { this.gardenContentArray = data; }
+    );
   }
 
 }
